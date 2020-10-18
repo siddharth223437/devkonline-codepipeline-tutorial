b0091482cd4f658bbe66424732379d2d94b6a39f
siddharth223437 <siddharthsarathe.nmims@gmail.com>
2020-10-18 15:33:44 -0500

    Initial commit

---
+++ src/main/java/com/devkonline/tutorial/DevkonlineTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.devkonline.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevkonlineTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevkonlineTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkonline/tutorial/DevkonlineTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkonline.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevkonlineTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
